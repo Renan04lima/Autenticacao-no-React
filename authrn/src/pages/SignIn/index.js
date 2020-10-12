30a30edbe1ad389623b6128dfb99e13083915552
Renan Oliveira <renan04lima@outlook.com>
2020-10-12 19:00:05 -0300

    :v: feat: pacotes da rn, navigation e add routes

---
+++ authrn/src/pages/SignIn/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Button, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+const SignIn = () => (
+  <View style={styles.container}>
+    <Button title="Sign In" onPress={() => {}} />
+  </View>
+);
+
+export default SignIn;
