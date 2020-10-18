92908d2494b10dfeda8a21690ce083873537e0e2
Renan Oliveira <renan04lima@outlook.com>
2020-10-18 20:51:28 -0300

    feat: AsyncStorage

--- authrn/src/contexts/auth.tsx
+++ authrn/src/contexts/auth.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import * as auth from "../services/auth";
+import AsyncStorage from '@react-native-community/async-storage';
 
 interface AuthContextData {
   signed: boolean;
@@ -13,14 +14,33 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 export const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<object | null>(null);
 
+  useEffect(() => {
+    async function loadStorageData() {
+      const storagedUser = await AsyncStorage.getItem('@RNAuth:user');
+      const storagedToken = await AsyncStorage.getItem('@RNAuth:token');
+
+      if (storagedUser && storagedToken) {
+        setUser(JSON.parse(storagedUser));
+      }
+    }
+
+    loadStorageData();
+  });
+
   async function signIn() {
     const response = await auth.signIn();
     setUser(response.user);
+
+    await AsyncStorage.setItem('@RNAuth:user', JSON.stringify(response.user));
+    await AsyncStorage.setItem('@RNAuth:token', response.token);
   }
 
-  function signOut() {
+  async function signOut() {
+    await AsyncStorage.clear();
     setUser(null);
   }
+
+  
 
   return (
     <AuthContext.Provider value={{ signed: !!user, user, signIn, signOut }}>
