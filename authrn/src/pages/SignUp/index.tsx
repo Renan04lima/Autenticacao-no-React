57366f8464cfece0820b8a290c87fa93c9aff47b
Renan Oliveira <renan04lima@outlook.com>
2020-10-24 15:22:09 -0300

    feat: screen SignUp

--- authrn/src/pages/SignUp/index.tsx
+++ authrn/src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import {useAuth} from '../../contexts/auth';
 import {useNavigation} from '@react-navigation/native';
 
 import {styles} from './styles';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const SignIn: React.FC = () => {
   const {signIn} = useAuth();
@@ -22,34 +23,42 @@ const SignIn: React.FC = () => {
     signIn();
   }
 
-  const {navigate} = useNavigation();
+  const {goBack} = useNavigation();
 
-  function handlerNavigateInSignUp() {
-    navigate('SignUp');
+  function handlerNavigateInLogin() {
+    goBack();
   }
 
   return (
     <Container>
       <Header transparent style={styles.header}>
-        <Title style={styles.color}>Login</Title>
+        <Button
+          transparent
+          style={styles.buttonBack}
+          onPress={handlerNavigateInLogin}>
+          <Icon name="chevron-back" size={30} color="#003e8b" />
+        </Button>
+        <Title style={styles.title}>Nova conta</Title>
+        <Button transparent style={styles.buttonBack}>
+          <Icon name="chevron-back" size={30} color="white" />
+        </Button>
       </Header>
       <Content padder contentContainerStyle={styles.content}>
         <View style={styles.boxLogin}>
-          <Text style={styles.text}>Acesse a sua conta</Text>
           <Item regular style={styles.input}>
-            <Input placeholder="Usuário" />
+            <Input placeholder="Nome" />
+          </Item>
+          <Item regular style={styles.input}>
+            <Input placeholder="Email" />
           </Item>
           <Item regular style={styles.input}>
             <Input placeholder="Senha" />
           </Item>
-          <Button block style={styles.button} onPress={handleSign}>
-            <Text>Entrar</Text>
-          </Button>
         </View>
       </Content>
       <View style={styles.footer}>
-        <Button block transparent onPress={handlerNavigateInSignUp}>
-          <Text>Não possui uma conta?</Text>
+        <Button block style={styles.button} onPress={handleSign}>
+          <Text>Cadastrar</Text>
         </Button>
       </View>
     </Container>
---
+++ authrn/src/pages/SignUp/styles.ts
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    color: '#003e8b',
+  },
+  buttonBack: {
+    // alignSelf: 'flex-start',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  boxLogin: {
+    // backgroundColor: "blue",
+    justifyContent: 'space-around',
+    flex: 0.5,
+  },
+  button: {
+    borderRadius: 5,
+  },
+  text: {
+    alignSelf: 'center',
+    fontSize: 25,
+  },
+  input: {
+    borderRadius: 5,
+  },
+  footer: {
+    padding: 10,
+    paddingBottom: 10,
+  },
+});
