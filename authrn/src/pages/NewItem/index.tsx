649c83198283d24c76951375566453278886ec20
Renan Oliveira <renan04lima@outlook.com>
2020-10-24 17:37:58 -0300

    feat: screen NewItem

---
+++ authrn/src/pages/NewItem/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Input,
+  Item,
+  Button,
+  Text,
+  Textarea,
+  Form,
+} from 'native-base';
+import {View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import {styles} from './styles';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const NewItem: React.FC = () => {
+  const {goBack} = useNavigation();
+
+  function handlerNavigateInDashboard() {
+    goBack();
+  }
+
+  function handlerNewItem() {
+    goBack();
+  }
+
+  return (
+    <Container>
+      <Header transparent style={styles.header}>
+        <Button
+          transparent
+          style={styles.buttonBack}
+          onPress={handlerNavigateInDashboard}>
+          <Icon name="chevron-back" size={30} color="#003e8b" />
+        </Button>
+        <Title style={styles.title}>Novo item</Title>
+        <Button transparent style={styles.buttonBack}>
+          <Icon name="chevron-back" size={30} color="white" />
+        </Button>
+      </Header>
+      <Content padder contentContainerStyle={styles.content}>
+        <View style={styles.boxLogin}>
+          <Item regular style={styles.input}>
+            <Input placeholder="Nome" />
+          </Item>
+          <Form>
+            <Textarea
+              style={styles.input}
+              underline
+              rowSpan={5}
+              bordered
+              placeholder="Descrição"
+            />
+          </Form>
+        </View>
+      </Content>
+      <View style={styles.footer}>
+        <Button block style={styles.button} onPress={handlerNewItem}>
+          <Text>Cadastrar</Text>
+        </Button>
+      </View>
+    </Container>
+  );
+};
+
+export default NewItem;
